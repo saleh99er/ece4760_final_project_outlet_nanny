7bf7f0478a994beda9abd0a15109a0091767f937
Saleh Hassen <sih28@cornell.edu>
2019-11-19 11:55:46 -0500

    removed tft code from main_4760_final.c , still builds

--- Big_board_pt_1_3_2/pic32_lab4.X/build/default/production/_ext/1472/main_4760_final.o.d
+++ Big_board_pt_1_3_2/pic32_lab4.X/build/default/production/_ext/1472/main_4760_final.o.d
@@ -1,6 +1,4 @@
 build/default/production/_ext/1472/main_4760_final.o:  \
  ../main_4760_final.c  \
 ../config_1_3_2.h  \
-../pt_cornell_1_3_2.h  \
- ../tft_master.h  \
-../tft_gfx.h 
+../pt_cornell_1_3_2.h 
