37392bfb311b750f881fa8a993fdef5aa41721da
Saleh Hassen <sih28@cornell.edu>
2019-11-22 07:56:33 -0500

    template html made

--- WebAppPlayground/Attempt3_d3js_with_Flask/flask_example.py
+++ WebAppPlayground/Attempt3_d3js_with_Flask/flask_example.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-	return "Hello, World!"
+	return render_template('index.html', title="Flask substituted this text")
 	
 if __name__ == "__main__":
 	app.run(debug=True)
