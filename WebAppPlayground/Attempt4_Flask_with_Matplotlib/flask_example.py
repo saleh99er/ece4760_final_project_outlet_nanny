0e2895fe4e70baf8fb337df7e60120461bc1a757
Saleh Hassen <sih28@cornell.edu>
2019-12-10 11:07:20 -0500

    a lot of changes for website, flask, and schematic *shrug*

--- WebAppPlayground/Attempt4_Flask_with_Matplotlib/flask_example.py
+++ WebAppPlayground/Attempt4_Flask_with_Matplotlib/flask_example.py
@@ -7,7 +7,11 @@ app_debug  = 0 #set 1 if wish to debug with web app
 @app.route('/index')
 def index():
 	return render_template('index.html', debug=app_debug, flask_check="Flask substituted this text")
-	
+
+@app.route('/debug')
+def pseudo_serial_terminal():
+	return render_template('pseudo_serial_term.html', debug=app_debug)
+
 if __name__ == "__main__":
-	app.run(debug=True, port=5002)
+	app.run(debug=True, port=5000)
 
